c725f32508fce8e4383c85fda1a369b0c67697e4
BrunoLeone <leonecastanha@gmail.com>
2024-11-07 21:40:21 -0300

    projeto concluído

---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,19 @@
+import MoviesCard from '../../components/MovieCard';
+import  Pagination from '../../components/Pagination';
+
+function Listing (){
+    return(
+      <>
+      <div className='container'>
+        <div className='row'>
+         <div className='col-sm-6 col- lg-4 col-xl-3 mb-3'>
+      <MoviesCard />
+         </div>
+        </div>
+      </div>
+      <Pagination />
+      </>
+    );
+}
+export default Listing;
+
